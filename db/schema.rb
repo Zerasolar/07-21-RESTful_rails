9a3a0b2dc90f5de2e006372e044cedaef89c9ade
Kristin Houchin <kmhouchin@gmail.com>
2015-07-21 13:14:35 -0500

    Forgot to remove the coffee and scss

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721175341) do
+ActiveRecord::Schema.define(version: 20150721180106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stories", force: :cascade do |t|
+    t.text    "title"
+    t.text    "summary"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.text     "email"
